9b2d8104c6a7afe3dc95e3932a6acab3daa1c97b
bertyjobbo <robjohnson1978@gmail.com>
2014-06-10 21:00:43 +0100

    Adding more excluded stuff

---
+++ Singular.Configuration/SingularApplicationBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Singular.Configuration
+{
+    /// <summary>
+    /// Base application class
+    /// </summary>
+    public abstract class SingularApplicationBase
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        protected SingularApplicationBase()
+        {
+            AdminSections = new List<SingularAdminSection>();
+        }
+
+        /// <summary>
+        /// Root configuration
+        /// </summary>
+        public SingularApplicationConfigurationRoot RootConfiguration { get; set; }
+
+        /// <summary>
+        /// Is master
+        /// </summary>
+        public bool IsMasterApplication { get; set; }
+
+        /// <summary>
+        /// Name of app
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Configure
+        /// </summary>
+        /// <param name="config"></param>
+        public abstract void Configure(SingularApplicationConfigurationRoot config);
+
+        /// <summary>
+        /// Application ID
+        /// </summary>
+        public abstract Guid ApplicationId { get; }
+
+        /// <summary>
+        /// Admin sections
+        /// </summary>
+        public IList<SingularAdminSection> AdminSections { get; private set; }
+    }
+}
---
+++ Singular.Configuration/SingularApplicationConfigurationRoot.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Singular.Configuration
+{
+    public class SingularApplicationConfigurationRoot
+    {
+        /// <summary>
+        /// Is master
+        /// </summary>
+        public SingularApplicationConfigurationRoot IsMasterApplication()
+        {
+            Application.IsMasterApplication = true;
+            return this;
+        }
+
+        /// <summary>
+        /// Application
+        /// </summary>
+        public SingularApplicationBase Application { get; set; }
+
+        /// <summary>
+        /// Has name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public SingularApplicationConfigurationRoot Named(string name)
+        {
+            if (!Application.IsMasterApplication) throw new Exception("Cannot name non-master application");
+
+            Application.Name = name;
+
+            return this;
+        }
+
+        public SingularApplicationConfigurationRoot WithAdminSection(Action<SingularAdminSection> action)
+        {
+            var section = new SingularAdminSection();
+            action.Invoke(section);
+            Application.AdminSections.Add(section);
+            return this;
+        }
+    }
+}
