4e37e443ae2ce0f1540f276c67c4a159cb67ee94
bertyjobbo <robjohnson1978@gmail.com>
2014-06-10 08:49:43 +0100

    Test commit

---
+++ MySingularApplication/Configuration/MyApplicationConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Singular.Configuration;
+
+namespace MySingularApplication.Configuration
+{
+    /// <summary>
+    /// My app config
+    /// </summary>
+    public class MyApplicationConfig : ISingularApplication
+    {
+
+    }
+}
---
+++ Singular.Configuration.Tests/FluentApiTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Singular.Configuration.Tests
+{
+    [TestClass]
+    public class FluentApiTests
+    {
+        [TestMethod]
+        public void Test_Instantiation()
+        {
+
+        }
+    }
+}
