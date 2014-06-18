d569bf00a2793d1657fce197fc24a38cb96c4745
bertyjobbo <robjohnson1978@gmail.com>
2014-06-18 19:34:59 +0100

    Successful test

--- Singular.Web.Admin.JsTests/test1.js
+++ Singular.Web.Admin.JsTests/test1.js
@@ -1,11 +1,15 @@
-﻿/// <reference path="c:\projects\git\singular\singular.web.admin.jstests\scripts\jasmine.js" />
-/// <reference path="c:\projects\git\singular\singular.web.admin.jstests\dummyclass.js" />
+﻿/// <reference path="scripts\jasmine.js" />
+/// <reference path="dummyclass.js" />
 
 
 describe("Test 1", function () {
 
-    it("Returns 1", function() {
-        expect(new HelloWorld().Init().toEqual(1));
+    it("Returns 1", function () {
+        var hw = new HelloWorld();
+        var initVal = hw.Init();
+        expect(initVal === 1);
+        expect(5 === 5);
+        expect(5 !== "5");
     });
 
 });
